9cf5d2979b3c1d2bf5cf8cc161216b070900f3ca
Dorell James Galang <galangdj@gmail.com>
2019-04-23 13:19:57 +0800

    Remove refereces from Layout component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/Bio"
-import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import moment from "moment"
 import { rhythm, scale } from "../utils/typography"
@@ -17,58 +16,56 @@ class BlogPostTemplate extends React.Component {
 
     return (
       <>
-        <Layout location={this.props.location} title={siteTitle}>
-          <SEO
-            title={post.frontmatter.title}
-            description={post.frontmatter.description || post.excerpt}
-          />
-          <h1>{post.frontmatter.title}</h1>
-          <p
-            style={{
-              ...scale(-1 / 5),
-              display: `block`,
-              marginBottom: rhythm(1),
-              marginTop: rhythm(-2 / 5),
-              color: `var(--textSecondary)`,
-            }}
-          >
-            {moment(post.frontmatter.date).format("MMMM DD, YYYY")}
-            <span> &middot; </span>
-            <em>{formatReadingTime(post.timeToRead)}</em>
-          </p>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
-          />
-          <Bio />
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <h1>{post.frontmatter.title}</h1>
+        <p
+          style={{
+            ...scale(-1 / 5),
+            display: `block`,
+            marginBottom: rhythm(1),
+            marginTop: rhythm(-2 / 5),
+            color: `var(--textSecondary)`,
+          }}
+        >
+          {moment(post.frontmatter.date).format("MMMM DD, YYYY")}
+          <span> &middot; </span>
+          <em>{formatReadingTime(post.timeToRead)}</em>
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <Bio />
 
-          <ul
-            style={{
-              display: `flex`, // @todo: hide when on mobile
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0,
-            }}
-          >
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </Layout>
+        <ul
+          style={{
+            display: `flex`, // @todo: hide when on mobile
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
         <BottomMenu previous={previous} next={next} />
       </>
     )
