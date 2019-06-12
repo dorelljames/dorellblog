9c8d39ed847638ed8677071e72580e990f2bc0ad
Dorell James Galang <galangdj@gmail.com>
2019-06-12 09:41:43 +0800

    Add temp content on projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,11 @@ import BottomMenu from "../components/BottomMenu/Index"
 
 const ProjectsPage = props => (
   <>
-    <p>Projects</p>
+    <p>I write code and I write and write and write and write.</p>
+
+    <h2>Recent Works</h2>
+    <p>KAYU: Kitchen + Bar</p>
+
     <BottomMenu location={props.location} />
   </>
 )
