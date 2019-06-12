0553a1c677b99a6f65d85e5dc6e714ad2c38a7b5
Dorell James Galang <galangdj@gmail.com>
2019-06-12 13:03:49 +0800

    Updated bottom menu

--- src/components/BottomMenu/Index.js
+++ src/components/BottomMenu/Index.js
@@ -29,7 +29,30 @@ const BottomMenuIndex = props => {
         </Link>
       </li>
       <li>
-        <Link to="/about">
+        <Link to="/projects">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={
+              props.location.pathname === "/projects"
+                ? `var(--textPrimary)`
+                : `var(--textInteractive)`
+            }
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-folder"
+          >
+            <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+          </svg>
+          <span>Projects</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/events">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -51,30 +74,7 @@ const BottomMenuIndex = props => {
             <line x1="9" y1="9" x2="9.01" y2="9" />
             <line x1="15" y1="9" x2="15.01" y2="9" />
           </svg>
-          <span>About</span>
-        </Link>
-      </li>
-      <li>
-        <Link to="/projects">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke={
-              props.location.pathname === "/projects"
-                ? `var(--textPrimary)`
-                : `var(--textInteractive)`
-            }
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-folder"
-          >
-            <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
-          </svg>
-          <span>Projects</span>
+          <span>Events</span>
         </Link>
       </li>
       <li>
