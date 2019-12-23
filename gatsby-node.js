92eb27cdc4fbce8ed6420e1b1982291f7ac6b410
Dorell James Galang <galangdj@gmail.com>
2019-12-23 09:49:28 +0800

    Fix pagination posts relevance

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,54 +3,77 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+  const createBlogPostsWithPagination = query => {
+    return graphql(query).then(result => {
+      if (result.errors) {
+        throw result.errors
+      }
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          filter: { frontmatter: { draft: { ne: true } } }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+      // Create blog posts pages.
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+          path: "blog" + post.node.fields.slug,
+          component: blogPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            categories: post.node.frontmatter.categories,
+            previous,
+            next,
+          },
+        })
+      })
+
+      return null
+    })
+  }
+
+  const blogQuery = `
+    {
+      allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, filter: {frontmatter: {categories: {eq: "Web Development"}, draft: {ne: true}}}) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              categories
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
     }
+  `
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+  const blogArchivesQuery = `
+    {
+      allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, filter: {frontmatter: {categories: {ne: "Web Development"}, draft: {ne: true}}}) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              categories
+            }
+          }
+        }
+      }
+    }
+  `
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: "blog" + post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-
-    return null
-  })
+  return Promise.all([
+    createBlogPostsWithPagination(blogQuery),
+    createBlogPostsWithPagination(blogArchivesQuery),
+  ])
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
