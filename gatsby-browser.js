121e07ebb92a7a1967e1116c81bd05de8b15c8c5
Dorell James Galang <galangdj@gmail.com>
2019-12-29 10:09:23 +0800

    Updated prism styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
 // custom typefaces
 import "typeface-lora"
 import "typeface-varela-round"
+
+// Prism styling
+require("prismjs/themes/prism.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
