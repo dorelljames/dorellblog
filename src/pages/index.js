d0a4ad7babd18041fb4e22be04aa1a8d87647285
Dorell James <galangdj@gmail.com>
2021-01-04 10:35:41 +0800

    Fix global graphql import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Emoji from "../components/Emoji"
 import SEO from "../components/SEO"
 import SocialMedia from "../components/SocialMedia"
