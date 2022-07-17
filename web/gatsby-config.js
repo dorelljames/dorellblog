4ecaa2bcda536209543bb8ee93efeb8a9eec4f87
Dorell James <galangdj@gmail.com>
2022-07-17 21:22:23 +0800

    update with GA4 tag

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -84,22 +84,11 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "UA-39533448-1", // Google Analytics / GA
+          "G-9Y1N7DRP5N",
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          head: true,
         },
       },
     },
